283e82c0f2a8eb1244e452ef0224bb8dc714969d
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-11-13 14:52:53 +0400

    WhatsCyber 1.0

---
+++ qr.js
@@ -0,0 +1,32 @@
+const chalk = require('chalk');
+const {WAConnection, MessageOptions, MessageType, Mimetype} = require('@adiwajshing/baileys');
+const {StringSession} = require('./whatscyber/');
+const fs = require('fs');
+
+async function whatsCyber () {
+    const conn = new WAConnection();
+    const Session = new StringSession();  
+    conn.version = [3, 3234, 9]
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 50000;
+
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Cyber')}
+${chalk.white.italic('WhatsCyber Kodu Alıcı')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+    });
+    conn.on('open', async () => {
+        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
+        console.log(
+            chalk.green.bold(conn.user.jid.startsWith('90') || conn.user.jid.startsWith('994') ? 'WhatsCyber String Kodunuz: ' : 'WhatsCyber String Code: '), st
+        );
+
+        if (!fs.existsSync('config.env')) {
+            fs.writeFileSync('config.env', `CYBER_SESSION="${st}"`);
+        }
+        console.log(conn.user.jid.startsWith('90') || conn.user.jid.startsWith('994') ? 'Bu Kodu Heçkəslə Paylaşma ' + conn.user.name : 'Dont Share This Code to Anyone ' + conn.user.name)
+        process.exit(0);
+    });
+    await conn.connect();
+}
+whatsCyber()
